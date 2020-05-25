0842a1868ff49f48c1dd75e8cf0f01eb1776be97
Michał Szkarłat <34009626+mikiisz@users.noreply.github.com>
2020-05-25 18:42:59 +0200

    add  __main__ handler

--- tests_onp.py
+++ tests_onp.py
@@ -25,24 +25,35 @@ class test_2_2(unittest.TestCase):
     def test_4_1_addition(self):
         assertThose(self, "2 2 +", "4\n")
 
+        
     def test_4_2_subtraction(self):
         assertThose(self, "2 2 -", "0\n")
 
+        
     def test_4_3_multiplication(self):
         assertThose(self, "2 2 *", "4\n")
 
+        
     def test_4_4_division(self):
         assertThose(self, "2 2 /", "1.0\n")
 
+        
     def test_4_5_cos(self):
         assertThose(self, "3.1415926535 cos", "-1.0\n")     
 
+        
     def test_4_6_sqrt(self):
         assertThose(self, "16 sqrt", "4.0\n")   
 
+        
     def test_4_7_advanced(self):
         assertThose(self, "12 2 3 4 * 10 5 / + * +", "40.0\n")  
 
+        
     def test_4_8_advanced(self):
         assertThose(self, "5 1 2 + 4 * + 3 -", "14\n")  
+        
+        
+if __name__ == '__main__':
+    unittest.main()
  
